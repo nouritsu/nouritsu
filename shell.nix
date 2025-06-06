36ef7604f354428fc9c885984cd4e9e2a210a32a
Aneesh Bhave <aneesh1701@gmail.com>
2025-06-06 16:13:31 +0000

    add a devshell with serve alias

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  packages = with pkgs.buildPackages; [
+    gcc
+    ruby_3_4
+    rubyPackages_3_4.jekyll
+    rubyPackages_3_4.jekyll-feed
+  ];
+
+  shellHook = ''
+    bundle install
+    alias serve="bundle exec jekyll serve"
+  '';
+}
