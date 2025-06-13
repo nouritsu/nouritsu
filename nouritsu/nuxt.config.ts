172191e7eb2a3ec2938a679723681e3e0f184be9
Aneesh Bhave <aneesh1701@gmail.com>
2025-06-13 16:46:08 +0000

    add scss preprocessors

--- nouritsu/nuxt.config.ts
+++ nouritsu/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+	compatibilityDate: "2025-05-15",
+	devtools: { enabled: true },
+	css: [
+		"@/assets/styles/main.scss", // main.scss already imports reset and theme
+	],
+});
