8b896916f6c117d8cd32d09a330e2b05e438acba
Aneesh Bhave <aneesh1701@gmail.com>
2025-06-14 07:22:56 +0000

    implement email obfuscation using b64 and build from parts

--- assets/js/link.ts
+++ assets/js/link.ts
@@ -4,14 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
 		return;
 	}
 
+	const href = link.getAttribute("href");
+	if (href && href.startsWith("mailto:")) {
+		let parts = href
+			.replace("mailto:", "")
+			.split(":")
+			.map((part) => atob(part));
+
+		if (parts.length === 3) {
+			const [usr, srv, dom] = parts;
+			link.setAttribute("href", `mailto:${usr}@${srv}.${dom}`);
+		}
+	}
+
 	const text = link.querySelector(".link-text");
 	if (!text) {
 		return;
 	}
 
 	const text_original = text.textContent || "";
-	const text_hover = link.getAttribute("hover-text") || text_original;
+	const text_hover = text.getAttribute("hover-text") || text_original;
 
+	// Current page based changes
 	const link_type = link.getAttribute("link-type");
 	if (link_type === "internal") {
 		const current_page_url = window.location.pathname;
@@ -35,6 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	const open_original = open.textContent || "[";
 	const close_original = close.textContent || "]";
 
+	// Hover based changes
 	if (open && text && close) {
 		link.addEventListener("mouseover", () => {
 			open.textContent = "(";
