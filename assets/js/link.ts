328674f7e38b6b484ae62ae82f82a463486a202e
Aneesh Bhave <aneesh1701@gmail.com>
2025-06-17 17:55:48 +0000

    update links API

--- assets/js/link.ts
+++ assets/js/link.ts
@@ -1,66 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const link = document.querySelector(".link");
-	if (!link) {
+	const links = document.querySelectorAll(".link");
+	if (!links || links.length === 0) {
 		return;
 	}
 
-	const href = link.getAttribute("href");
-	if (href && href.startsWith("mailto:")) {
-		let parts = href
-			.replace("mailto:", "")
-			.split(":")
-			.map((part) => atob(part));
+	links.forEach((link) => {
+		const href = link.getAttribute("href");
+		if (href && href.startsWith("mailto:")) {
+			let parts = href.replace("mailto:", "").split(":");
 
-		if (parts.length === 3) {
-			const [usr, srv, dom] = parts;
+			if (parts.length !== 3) {
+				return;
+			}
+
+			const [usr, srv, dom] = parts.map((part) => atob(part));
 			link.setAttribute("href", `mailto:${usr}@${srv}.${dom}`);
 		}
-	}
 
-	const text = link.querySelector(".link-text");
-	if (!text) {
-		return;
-	}
+		const text = link.querySelector(".link-text");
+		if (!text) {
+			return;
+		}
 
-	const text_original = text.textContent || "";
-	const text_hover = text.getAttribute("hover-text") || text_original;
+		const text_original = text.textContent || "";
+		const text_hover = text.getAttribute("hover-text") || text_original;
 
-	// Current page based changes
-	const link_type = link.getAttribute("link-type");
-	if (link_type === "internal") {
-		const current_page_url = window.location.pathname;
-		if (current_page_url.toLowerCase().includes(text_original.toLowerCase())) {
-			const open = link.querySelector(".bracket-open");
-			const close = link.querySelector(".bracket-close");
+		// Current page based changes
+		const link_type = link.getAttribute("link-type");
+		if (link_type === "internal") {
+			const current_page_url = window.location.pathname;
+			if (
+				current_page_url.toLowerCase().includes(text_original.toLowerCase())
+			) {
+				const open = link.querySelector(".bracket-open");
+				const close = link.querySelector(".bracket-close");
 
-			if (open && close) {
-				open.textContent = "(";
-				close.textContent = ")";
+				if (open && close) {
+					open.textContent = "(";
+					close.textContent = ")";
+				}
 			}
 		}
-	}
 
-	const open = link.querySelector(".bracket-open");
-	const close = link.querySelector(".bracket-close");
-	if (!open || !close) {
-		return;
-	}
+		const open = link.querySelector(".bracket-open");
+		const close = link.querySelector(".bracket-close");
+		if (!open || !close) {
+			return;
+		}
 
-	const open_original = open.textContent || "[";
-	const close_original = close.textContent || "]";
+		const open_original = open.textContent || "[";
+		const close_original = close.textContent || "]";
 
-	// Hover based changes
-	if (open && text && close) {
-		link.addEventListener("mouseover", () => {
-			open.textContent = "(";
-			text.textContent = text_hover;
-			close.textContent = ")";
-		});
+		// Hover based changes
+		if (open && text && close) {
+			link.addEventListener("mouseover", () => {
+				open.textContent = "(";
+				text.textContent = text_hover;
+				close.textContent = ")";
+			});
 
-		link.addEventListener("mouseout", () => {
-			open.textContent = open_original;
-			text.textContent = text_original;
-			close.textContent = close_original;
-		});
-	}
+			link.addEventListener("mouseout", () => {
+				open.textContent = open_original;
+				text.textContent = text_original;
+				close.textContent = close_original;
+			});
+		}
+	});
 });
